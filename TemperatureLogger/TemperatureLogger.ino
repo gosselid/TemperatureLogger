5400b1d8075ea2e4cfa0697a318f9c38eb66353c
gosselid <34027860+gosselid@users.noreply.github.com>
2018-01-07 15:31:21 +0100

    Logging OK

---
+++ TemperatureLogger/TemperatureLogger.ino
@@ -0,0 +1,145 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+#define ECHO_TO_SERIAL   1 // echo data to serial port
+#define tempPin 0                // analog 0
+#define intertemps 1000*60*5-30
+
+RTC_PCF8523 RTC; // define the Real Time Clock object
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  while(1);
+}
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println();
+
+  // initialize the SD card
+  Serial.print("Initializing SD card...");
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+  // connect to RTC
+  Wire.begin();  
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+  }
+  
+
+  logfile.println("millis,stamp,datetime,temp");    
+#if ECHO_TO_SERIAL
+  Serial.println("millis,stamp,datetime,temp");
+#endif //ECHO_TO_SERIAL
+
+}
+
+void loop(void)
+{
+  DateTime now;
+  
+  // log milliseconds since starting
+  uint32_t m = millis();
+  logfile.print(m);           // milliseconds since start
+  logfile.print(", ");    
+#if ECHO_TO_SERIAL
+  Serial.print(m);         // milliseconds since start
+  Serial.print(", ");  
+#endif
+
+  // fetch the time
+  now = RTC.now();
+  // log time
+  logfile.print('"');
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+  logfile.print('"');
+#if ECHO_TO_SERIAL
+  Serial.print('"');
+  Serial.print(now.year(), DEC);
+  Serial.print("/");
+  Serial.print(now.month(), DEC);
+  Serial.print("/");
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+  Serial.print('"');
+#endif //ECHO_TO_SERIAL
+  
+  analogRead(tempPin); 
+  delay(10);
+  int tempReading = analogRead(tempPin);    
+  
+  double temperatureC=map(analogRead(A0),0,1023,0,5000)/10.0;
+  
+  logfile.print(", ");    
+  logfile.print(temperatureC);
+#if ECHO_TO_SERIAL
+  Serial.print(", ");    
+  Serial.print(temperatureC);
+#endif //ECHO_TO_SERIAL
+
+  logfile.println();
+#if ECHO_TO_SERIAL
+  Serial.println();
+#endif // ECHO_TO_SERIAL
+  
+  logfile.flush();
+  
+  delay(intertemps);
+  
+}
+
+
